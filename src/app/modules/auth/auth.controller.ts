6cf4d91950760ea550f41de8f12985d3ed23b7f5
unknown <nishanahmed13913@gmail.com>
2026-03-11 16:50:36 +0600

    modified google login redirect system

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -11,12 +11,12 @@ import { JwtPayload } from 'jsonwebtoken';
 import env from '../../config/env';
 import { SendResponse } from '../../utils/SendResponse';
 import { authService } from './auth.service';
-
+import { clearGlobalAppDefaultCred } from 'firebase-admin/lib/app/credential-factory';
 
 // REGISTER WITH GOOGLE
 const googleRegister = CatchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const redirect = (req.query?.redirect as string) || '/';
+    const redirect = (req.query?.redirectTo as string) || '/';
 
     passport.authenticate('google', {
       scope: ['profile', 'email'],
@@ -25,7 +25,6 @@ const googleRegister = CatchAsync(
     })(req, res, next);
   }
 );
-
 
 //  GOOGLE CALLBACK
 const googleCallback = CatchAsync(
@@ -37,12 +36,27 @@ const googleCallback = CatchAsync(
 
     const user = req.user as JwtPayload;
     if (!user) throw new AppError(httpStatus.BAD_REQUEST, 'User not found');
-
     const token = await createUserTokens(user);
-    res.redirect(`${env.FRONTEND_URL}/${redirectTo}?access=${token.accessToken}&refresh=${token.refreshToken}`); // Redirected to frontend url (With specific Routes)
+
+    const userAgent = req.headers['user-agent'] || '';
+
+    const isAndroid = /android/i.test(userAgent);
+    const isIOS = /iphone|ipad|ipod/i.test(userAgent);
+
+
+    if (isAndroid || isIOS) {
+      res.redirect(
+        `${env.DEEP_LINK}?access=${token.accessToken}&refresh=${token.refreshToken}`
+      );
+    }
+    
+
+    res.redirect(
+        `${env.FRONTEND_URL}/shop-overview?access=${token.accessToken}&refresh=${token.refreshToken}`
+      );
+
   }
 );
-
 
 // CREDENTIAL LOGIN
 const credentialsLogin = CatchAsync(
@@ -60,84 +74,92 @@ const credentialsLogin = CatchAsync(
         success: true,
         statusCode: httpStatus.OK,
         message: 'Login success',
-        data: userTokens
+        data: userTokens,
       });
     })(req, res, next);
   }
 );
 
-
 // CHANGE PASSWORD
-const changePassword = CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const { userId } = req.user as JwtPayload;
-  const { oldPassword, newPassword } = req.body;
-  await authService.changePasswordService(userId, oldPassword, newPassword);
-  
-  SendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: "Password has been changed",
-    data: null
-  })
-})
+const changePassword = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userId } = req.user as JwtPayload;
+    const { oldPassword, newPassword } = req.body;
+    await authService.changePasswordService(userId, oldPassword, newPassword);
 
+    SendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Password has been changed',
+      data: null,
+    });
+  }
+);
 
 // FORGET PASSWORD
-const forgetPassword = CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const { email } = req.params;
-  const result = await authService.forgetPasswordService(email as string);
+const forgetPassword = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email } = req.params;
+    const result = await authService.forgetPasswordService(email as string);
 
-  SendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: "Password reset OTP sent",
-    data: result
-  })
-})
-
-
-// VERIFY FORGET PASSWORD OTP
-const verifyForgetPasswordOTP = CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const { email, otp } = req.body;
-  const result = await authService.verifyForgetPasswordOTPService(email as string, otp);
-
-  SendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: "OTP verified",
-    data: result
-  })
-})
-
+    SendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Password reset OTP sent',
+      data: result,
+    });
+  }
+);
 
 // VERIFY FORGET PASSWORD OTP
-const resetPassword = CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.token as string;
-  
-  const { newPassword } = req.body;
-  const result = await authService.resetPasswordService(token, newPassword);
+const verifyForgetPasswordOTP = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, otp } = req.body;
+    const result = await authService.verifyForgetPasswordOTPService(
+      email as string,
+      otp
+    );
 
-  SendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: "Password reset success",
-    data: result
-  })
-})
-
+    SendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'OTP verified',
+      data: result,
+    });
+  }
+);
 
 // VERIFY FORGET PASSWORD OTP
-const getNewAccessToken = CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const { refreshToken } = req.body;
-  const result = await authService.getNewAccessTokenService(refreshToken);
+const resetPassword = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.token as string;
 
-  SendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: "New access token generated",
-    data: result
-  })
-})
+    const { newPassword } = req.body;
+    const result = await authService.resetPasswordService(token, newPassword);
+
+    SendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Password reset success',
+      data: result,
+    });
+  }
+);
+
+// VERIFY FORGET PASSWORD OTP
+const getNewAccessToken = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { refreshToken } = req.body;
+    const result = await authService.getNewAccessTokenService(refreshToken);
+
+    SendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'New access token generated',
+      data: result,
+    });
+  }
+);
 
 export const authController = {
   googleRegister,
@@ -147,5 +169,5 @@ export const authController = {
   forgetPassword,
   verifyForgetPasswordOTP,
   resetPassword,
-  getNewAccessToken
+  getNewAccessToken,
 };
