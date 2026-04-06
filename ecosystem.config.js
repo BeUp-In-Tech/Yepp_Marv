a4ca7a25bbf0362969c8903838949951f6940149
unknown <nishanahmed13913@gmail.com>
2026-04-06 13:40:52 +0600

    configured ecosystem file to start the server and worker file by one command

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+module.exports = {
+  apps: [
+    {
+      name: 'server',
+      script: './dist/server.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      autorestart: true,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'Worker',
+      script: './dist/app/queue/worker.js',
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      autorestart: true,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
