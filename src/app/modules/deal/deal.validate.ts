44410c90829b41ed78884d8e08ddc19186ef419f
unknown <nishanahmed13913@gmail.com>
2026-04-23 10:15:52 +0600

    added meaningful zod validation error message

--- src/app/modules/deal/deal.validate.ts
+++ src/app/modules/deal/deal.validate.ts
@@ -8,13 +8,13 @@ export const CreateDealZodSchema = z
   .object({
     category: objectId,
 
-    title: z.string().min(2).max(120).trim(),
-    reguler_price: z.number().nonnegative(),
-    discount: z.number().min(0).max(100).default(0), // percent
+    title: z.string().min(5, "Title must be minimum 5 characters").max(120, "Title must be maximum 120 characters").trim(),
+    reguler_price: z.number("Price must be number").nonnegative("Regular price shouldn't be negative number"),
+    discount: z.number("Discount must be number").min(0, "Minimum discount should be 0").max(100, "Maximum discount should be 100%").default(0), // percent
 
-    highlight: z.array(z.string().min(1).max(120)).max(20).default([]),
-    tags: z.array(z.string().min(1).max(50)).max(20).default([]),
-    description: z.string("Description must be string").min(10).max(5000).trim(),
+    highlight: z.array(z.string("Highlight should be string").min(1, "Highlight should be minimum 1 character").max(120, "Maximum highlight length should be 120 characters")).max(20).default([]),
+    tags: z.array(z.string().min(1, "Minimum 1 tags required").max(50, "Maximum 50 tags allowed")).default([]),
+    description: z.string("Description must be string").min(10, "Description must be minimum 10 characters").max(5000, "Max description length should be 5000 characters").trim(),
     images: z.array(z.string().url()),
     coupon: z.string("Coupon must be string"),
     available_in_outlet: z.array(z.string()),
@@ -27,21 +27,21 @@ export const CreateDealZodSchema = z
 
 // UPDATE ZOD SCHEMA
 export const UpdateDealZodSchema = z.object({
-  title: z.string("Title must be string").min(2).max(120).trim().optional(),
-  reguler_price: z.number("Reguler price must be number").nonnegative().optional(),
-  discount: z.number("Discount must be number").min(0).max(100).optional(),
-  highlight: z.array(z.string("Highlight must be string").min(1).max(120)).max(20).optional(),
-  deletedHighlights: z.array(z.string().min(1).max(120)).max(20).optional(),
-  tags: z.array(z.string().min(1).max(50)).max(20).default([]).optional(),
-  deletedTags: z.array(z.string().min(1).max(120)).max(20).optional(),
-  images: z.array(z.string().url()).optional(),
-  description: z.string().min(10).max(5000).trim().optional(),
-  deletedImages: z.array(z.string().url()).optional(), // Images should be an array of valid URLs
+  title: z.string("Title must be string").min(2, "Title must be minimum 2 characters").max(120, "Title must be maximum 120 characters").trim().optional(),
+  reguler_price: z.number("Reguler price must be number").nonnegative("Regular price shouldn't be negative number").optional(),
+  discount: z.number("Discount must be number").min(0, "Minimum discount should be 0").max(100, "Maximum discount should be 100%").optional(),
+  highlight: z.array(z.string("Highlight must be string").min(1, "Highlight should be minimum 1 character").max(120, "Maximum highlight length should be 120 characters")).max(20, "Maximum 20 highlights allowed").optional(),
+  deletedHighlights: z.array(z.string("Deleted highlight must be string").min(1, "Deleted highlight should be minimum 1 character").max(120, "Maximum deleted highlight length should be 120 characters")).max(20, "Maximum 20 deleted highlights allowed").optional(),
+  tags: z.array(z.string("Tag must be string").min(1, "Minimum 1 tags required").max(50, "Maximum 50 tags allowed")).max(20, "Maximum 20 tags allowed").default([]).optional(),
+  deletedTags: z.array(z.string("Deleted tag must be string").min(1, "Deleted tag should be minimum 1 character").max(120, "Maximum deleted tag length should be 120 characters")).max(20, "Maximum 20 deleted tags allowed").optional(),
+  images: z.array(z.string("Image must be string").url("Image must be a valid URL")).optional(),
+  description: z.string("Description must be string").min(10, "Description must be minimum 10 characters").max(5000, "Max description length should be 5000 characters").trim().optional(),
+  deletedImages: z.array(z.string("Deleted image must be string").url("Deleted image must be a valid URL")).optional(), // Images should be an array of valid URLs
   coupon: z
     .string("Coupon must be string")
     .optional(),
   coupon_option: z.object({
-      qr: z.string().url().optional(),
-      upc: z.string().url().optional(),
+      qr: z.string("QR must be string").url("QR must be a valid URL").optional(),
+      upc: z.string("UPC must be string").url("UPC must be a valid URL").optional(),
     }).optional()
 });
